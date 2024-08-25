940ba91a38280ae096d7df94f0b6003507c937d3
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-08-25 22:39:39 +0200

    chore: setup backend server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,18 @@
 import express, { Request, Response } from "express";
-import mongoose from "mongoose";
+import connectDB from "./config/db";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+
+connectDB();
+
+app.use(express.json());
+app.get("/", (req: Request, res: Response) => {
+  res.send("Backend is running!");
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
