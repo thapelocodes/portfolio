ef79bbc5389a91efd82ee6c9ef694bd95ef5e8c9
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-09-01 13:08:47 +0200

    feat(ui): add hover animation to the  services section links

--- frontend/src/components/ServicesSection.tsx
+++ frontend/src/components/ServicesSection.tsx
@@ -25,32 +25,32 @@ const ServicesSection = () => {
     <section className="services w-full rounded-2xl my-2 md:max-w-screen-sm mx-auto py-2 px-3 overflow-hidden">
       <h2 className="text-xl m-2">Services & Skills</h2>
       <div className="flex flex-col md:flex-row justify-between-2 md:whitespace-nowrap scrollContainer">
-        <div className="w-fit my-1 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1">
+        <div className="w-fit my-1 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1 transform transition-transform duration-300 hover:scale-110">
           <Link href="/services/web-dev" className="">
             Web Development
           </Link>
         </div>
-        <div className="w-fit my-1 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1">
+        <div className="w-fit my-1 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1 transform transition-transform duration-300 hover:scale-110">
           <Link href="/services/app-dev" className="">
             App Development
           </Link>
         </div>
-        <div className="w-fit mt-1 mb-2 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1">
+        <div className="w-fit my-1 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1 transform transition-transform duration-300 hover:scale-110">
           <Link href="/services/frontend" className="">
             Frontend Development
           </Link>
         </div>
-        <div className="w-fit mt-1 mb-2 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1">
+        <div className="w-fit my-1 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1 transform transition-transform duration-300 hover:scale-110">
           <Link href="/services/api-dev" className="">
             API Development and Integration
           </Link>
         </div>
-        <div className="w-fit mt-1 mb-2 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1">
+        <div className="w-fit my-1 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1 transform transition-transform duration-300 hover:scale-110">
           <Link href="/services/seo" className="">
             Technical SEO
           </Link>
         </div>
-        <div className="w-fit mt-1 mb-2 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1">
+        <div className="w-fit my-1 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1 transform transition-transform duration-300 hover:scale-110">
           <Link href="/services/ui-ux" className="">
             UI/UX Design & Development
           </Link>
@@ -60,32 +60,32 @@ const ServicesSection = () => {
           /* Duplicate for seamless scrolling */
           windowSize > 750 && (
             <>
-              <div className="w-fit my-1 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1">
+              <div className="w-fit my-1 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1 transform transition-transform duration-300 hover:scale-110">
                 <Link href="/services/web-dev" className="">
                   Web Development
                 </Link>
               </div>
-              <div className="w-fit my-1 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1">
+              <div className="w-fit my-1 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1 transform transition-transform duration-300 hover:scale-110">
                 <Link href="/services/app-dev" className="">
                   App Development
                 </Link>
               </div>
-              <div className="w-fit mt-1 mb-2 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1">
+              <div className="w-fit my-1 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1 transform transition-transform duration-300 hover:scale-110">
                 <Link href="/services/frontend" className="">
                   Frontend Development
                 </Link>
               </div>
-              <div className="w-fit mt-1 mb-2 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1">
+              <div className="w-fit my-1 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1 transform transition-transform duration-300 hover:scale-110">
                 <Link href="/services/api-dev" className="">
                   API Development and Integration
                 </Link>
               </div>
-              <div className="w-fit mt-1 mb-2 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1">
+              <div className="w-fit my-1 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1 transform transition-transform duration-300 hover:scale-110">
                 <Link href="/services/seo" className="">
                   Technical SEO
                 </Link>
               </div>
-              <div className="w-fit mt-1 mb-2 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1">
+              <div className="w-fit my-1 mx-auto md:mx-1 rounded-xl bg-gradient-to-tl from-slate-500 dark:from-slate-700 p-1 transform transition-transform duration-300 hover:scale-110">
                 <Link href="/services/ui-ux" className="">
                   UI/UX Design & Development
                 </Link>
@@ -94,7 +94,7 @@ const ServicesSection = () => {
           )
         }
       </div>
-      <div className="w-fit mx-auto bg-gradient-to-br from-blue-200 to-slate-500 dark:from-blue-950 dark:to-slate-900 dark:hover:from-blue-900 dark:hover:to-slate-800 rounded-xl my-1 py-2 px-4">
+      <div className="w-fit mx-auto bg-gradient-to-br from-blue-200 to-slate-500 dark:from-blue-950 dark:to-slate-900 dark:hover:from-blue-900 dark:hover:to-slate-800 rounded-xl my-1 py-2 px-4 transform transition-transform duration-300 hover:scale-110">
         <Link href="/services">See more services</Link>
       </div>
     </section>
