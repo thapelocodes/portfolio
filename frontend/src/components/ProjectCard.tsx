c6ed543f4422d8894b028d33a0f361483fbc6e30
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-09-01 13:09:25 +0200

    feat(ui): add hover animation to the project card links

--- frontend/src/components/ProjectCard.tsx
+++ frontend/src/components/ProjectCard.tsx
@@ -11,11 +11,19 @@ const ProjectCard = ({
   liveLink,
   githubLink,
 }: ProjectCardProps) => (
-  <div className="w-full md:w-1/3 mx-auto md:mx-1 project-card flex flex-col my-2 p-2 bg-gradient-to-br from-blue-100 to-slate-2300 dark:from-blue-950 dark:to-slate-950 rounded-xl text-center">
+  <div className="w-fit md:w-1/3 mx-auto md:mx-1 project-card flex flex-col my-2 p-2 bg-gradient-to-br from-blue-100 to-slate-2300 dark:from-blue-950 dark:to-slate-950 rounded-xl">
     <h3 className="text-lg mb-4">{name}</h3>
-    <p className="h-40">{description}</p>
-    <a href={liveLink}>Live Demo</a>
-    <a href={githubLink}>GitHub</a>
+    <div className="pb-2">
+      <p className="">{description}</p>
+    </div>
+    <div className="flex md:flex-col w-fit mx-auto py-2">
+      <div className="mx-1 w-24 md:my-1 transform transition-transform duration-200 hover:scale-110 bg-gradient-to-tl dark:from-cyan-950 dark:to-slate-950 dark:hover:from-cyan-800 p-1 rounded-lg">
+        <a href={liveLink}>Live Demo</a>
+      </div>
+      <div className="mx-1 w-24 md:my-1 transform transition-transform duration-200 hover:scale-110 bg-gradient-to-tl dark:from-cyan-950 dark:to-slate-950 dark:hover:from-cyan-800 p-1 rounded-lg">
+        <a href={githubLink}>GitHub</a>
+      </div>
+    </div>
   </div>
 );
 
