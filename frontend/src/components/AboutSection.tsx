83a993cf38774374b0bdeb3e9e51435986c1e0f5
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-09-01 13:06:20 +0200

    feat(ui): add hover animation to about section

--- frontend/src/components/AboutSection.tsx
+++ frontend/src/components/AboutSection.tsx
@@ -24,7 +24,7 @@ const AboutSection = () => (
       obtained my online certification from ALX Africa with a plethora of
       technical and soft skills.
     </p>
-    <button className="mt-2 py-2 px-4 bg-gradient-to-tl from-blue-200 to-slate-500 dark:from-cyan-900 dark:to-slate-950 dark:hover:from-cyan-900 dark:hover:to-slate-900 rounded-lg">
+    <button className="mt-2 py-2 px-4 bg-gradient-to-tl from-blue-200 to-slate-500 dark:from-cyan-900 dark:to-slate-950 dark:hover:from-cyan-900 dark:hover:to-slate-900 rounded-lg transform transition-transform duration-200 hover:scale-110">
       <Link href="/about">See more about me</Link>
     </button>
   </section>
