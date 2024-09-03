f0b66b6d0bd559134477113cb93771c9a0376429
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-09-03 04:02:09 +0200

    style: change blog card colorway in light theme

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -6,7 +6,7 @@ type BlogCardProps = {
 };
 
 const BlogCard = ({ title, content }: BlogCardProps) => (
-  <div className="w-full md:max-w-lg mx-auto blog-card my-2 p-2 bg-gradient-to-br from-blue-100 to-slate-2300 dark:from-blue-950 dark:to-slate-950 rounded-xl transform transition-transform duration-200 hover:scale-110">
+  <div className="w-full md:max-w-lg mx-auto blog-card my-2 p-2 bg-gradient-to-br from-slate-300 to-slate-500 dark:from-blue-950 dark:to-slate-950 rounded-xl transform transition-transform duration-200 hover:scale-110">
     <h3 className="text-lg p-1">{title}</h3>
     <p className="truncate p-2">{content}</p>
   </div>
