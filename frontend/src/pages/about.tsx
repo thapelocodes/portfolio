daa4ff48f41bd363d72ff0549012bcba6fc16a1d
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-09-03 21:50:51 +0200

    style: add margin, padding and change heading font size

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -3,18 +3,17 @@ import Footer from "@/components/Footer";
 import Image from "next/image";
 
 const imageStyle = {
-  width: 200,
-  height: 200,
+  maxWidth: "50%",
   overflow: "hidden",
   borderRadius: "100% 50% 70% 100% / 60% 60% 100% 100%",
 };
 
 const About = () => (
-  <div className="h-screen">
+  <div className="">
     <Header />
-    <main className="p-8 pt-20">
-      <h1 className="text-2xl mb-2">About Me</h1>
-      <div style={imageStyle} className="mx-auto">
+    <main className="p-8 pt-20 w-fit mx-auto md:max-w-screen-sm h-screen">
+      <h1 className="text-2xl mb-5">About Me</h1>
+      <div style={imageStyle} className="mx-auto my-5">
         <Image
           src="/M-9.jpg"
           alt="Thapelo M. Moumakoe"
@@ -22,29 +21,31 @@ const About = () => (
           height={3600}
         />
       </div>
-      <p>
+      <p className="my-2">
         I am a self-taught developer based in Pretoria, South Africa. I recently
         obtained my certification from ALX Africa with a plethora of technical
         and soft skills.
       </p>
-      <p>
-        Here&apos;s my resume:{" "}
+      <p className="my-2">
+        See my resume{" "}
         <a
           href="https://docs.google.com/document/d/1VyPjNeAOjvbi7CqVomPRmkEvAHnICqhyoIzIk7Mk4Gw/edit?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
+          className="hover:text-blue-500"
         >
-          Resume
+          here
         </a>
       </p>
-      <p>
-        Here&apos;s my certificate:{" "}
+      <p className="my-2">
+        See my certificate{" "}
         <a
           href="https://drive.google.com/file/d/10qXN1X57JYn9VoVLMsOy2AZFp6K_1duE/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
+          className="hover:text-blue-500"
         >
-          Certificate
+          here
         </a>
       </p>
     </main>
