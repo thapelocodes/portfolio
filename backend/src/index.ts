44f4af9a18c711e074e52a894bcf42efdc88f135
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-09-04 21:23:41 +0200

    chore: add routes to backend app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import connectDB from "./config/db";
 import dotenv from "dotenv";
+import blogRoutes from "./routes/blogRoutes";
+import projectRoutes from "./routes/projectRoutes";
 
 dotenv.config();
 
@@ -13,6 +15,8 @@ app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
   res.send("Backend is running!");
 });
+app.use("/api/blogs", blogRoutes);
+app.use("/api/projects", projectRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
