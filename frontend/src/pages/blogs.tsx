e3ffe0eb1f13e4c7710164501347264eb825c063
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-09-06 02:17:06 +0200

    chore: add logging to blogs page

--- frontend/src/pages/blogs.tsx
+++ frontend/src/pages/blogs.tsx
@@ -13,6 +13,7 @@ const Blogs = () => {
     const fetchBlogs = async () => {
       try {
         const response = await api.get("/blogs");
+        console.log("Blogs response: ", response);
         setBlogs(response.data);
       } catch (err) {
         console.error("Error fetching blogs: ", err);
@@ -27,41 +28,21 @@ const Blogs = () => {
       <Header />
       <main className="pt-20 h-screen">
         <h1 className="text-2xl">Blogs</h1>
-        {/* {blogs.map((blog: any) => (
-          <BlogCard
-            key={blog.id}
-            title={blog.title}
-            content={blog.content}
-            createdAt={blog.createdAt}
-            updatedAt={blog.updatedAt}
-          />
-        ))} */}
-        <div>
-          <Link href="blogs/blog1">
-            <BlogCard
-              title="Blog 1"
-              content="This is my first blog. Lorem ipsum odor amet, consectetuer adipiscing elit. Netus phasellus laoreet ut a libero? Rutrum suscipit natoque cursus nisi interdum hac. Vulputate aliquam tempus feugiat egestas diam sociosqu tristique lacinia duis. Rutrum faucibus dui etiam magna est consequat congue felis lacinia. Nascetur ridiculus vel vivamus egestas varius in blandit. Cubilia erat aptent hac vel, tortor torquent. Nullam eleifend nec, sed integer semper volutpat? Eget vivamus eleifend mauris nisi per in fringilla."
-              createdAt="1st September 2024"
-              updatedAt="1st September 2024"
-            />
-          </Link>
-          <Link href="blogs/blog2">
-            <BlogCard
-              title="Blog 2"
-              content="This is my second blog. Adapibus varius facilisis mollis hendrerit velit. Mus sociosqu semper aliquam imperdiet lectus ridiculus rutrum nascetur etiam. Dui natoque senectus eleifend sociosqu aliquam pellentesque mus metus dictum. Purus ut consequat congue platea lobortis. Tortor nulla lacinia blandit tincidunt egestas fames aliquet tortor habitasse. Aptent risus id adipiscing venenatis nulla ultrices. Dapibus ligula mollis lacus etiam urna posuere sollicitudin justo suspendisse? Primis felis purus cras nam taciti commodo a."
-              createdAt="1st September 2024"
-              updatedAt="1st September 2024"
-            />
-          </Link>
-          <Link href="blogs/blog3">
-            <BlogCard
-              title="Blog 3"
-              content="This is my third blog. Eget phasellus eleifend neque penatibus porta quam in? Phasellus nascetur velit pellentesque neque enim magnis. Mollis bibendum ridiculus fermentum aenean, vivamus porta vehicula. Fermentum velit malesuada torquent etiam amet. Pharetra fermentum eget nisi sed; id aliquam aenean? Consequat taciti commodo nibh rutrum justo fames facilisi. Pretium adipiscing ultrices ex metus donec."
-              createdAt="1st September 2024"
-              updatedAt="1st September 2024"
-            />
-          </Link>
-        </div>
+        {blogs.length > 0 ? (
+          blogs.map((blog: any) => (
+            <Link key={blog._id} href={`/blogs/${blog._id}`}>
+              <BlogCard
+                key={blog._id}
+                title={blog.title}
+                content={blog.content}
+                createdAt={new Date(blog.createdAt).toLocaleDateString()}
+                updatedAt={new Date(blog.updatedAt).toLocaleDateString()}
+              />
+            </Link>
+          ))
+        ) : (
+          <p>No blogs available.</p>
+        )}
       </main>
       <Footer />
     </div>
