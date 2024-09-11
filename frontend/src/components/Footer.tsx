2cb7599faf9a439915484d22129eeae8af90ee64
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-09-11 14:01:33 +0200

    fix(rehydration): adjust dom structure

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,8 +2,7 @@ const Footer = () => (
   <footer className="bg-slate-400 dark:bg-slate-900 h-24 flex items-center">
     <div className="w-fit mx-auto">
       <p className="m-3 text-secondary flex">
-        &copy; {new Date().getFullYear()}{" "}
-        <p className="text-primary ml-1">thapelocodes</p>
+        &copy; {new Date().getFullYear()} thapelocodes
       </p>
     </div>
   </footer>
