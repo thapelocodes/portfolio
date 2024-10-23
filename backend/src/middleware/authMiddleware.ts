3f1987fcc56179f44732bd7560e18448608eab8e
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-10-23 07:54:19 +0200

    fix: use type any for development ease

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -1,11 +1,7 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 
-export const authenticateAdmin = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const authenticateAdmin = (req: any, res: any, next: any) => {
   const token = req.headers.authorization?.split(" ")[1]; // Extract token from the "Bearer <token>" format
 
   if (!token) {
@@ -16,7 +12,7 @@ export const authenticateAdmin = (
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-    req.admin._id = decoded.id; // Attach admin info to request object
+    req.admin._id = decoded; // Attach admin info to request object
     next(); // Proceed to the next middleware or route handler
   } catch (error) {
     return res.status(401).json({ message: "Invalid token" });
