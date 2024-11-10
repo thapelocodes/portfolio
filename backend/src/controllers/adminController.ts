9fd330a921caada8d016d5ecc066461beaff4a3f
thapelocodes <123243154+tm-moumakoe@users.noreply.github.com>
2024-11-10 17:30:43 +0200

    chore: ensure access token expires after 1 day

--- backend/src/controllers/adminController.ts
+++ backend/src/controllers/adminController.ts
@@ -3,25 +3,25 @@ import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import Admin from "../models/Admin";
 
+const generateAccessToken = (id: string) => {
+  return jwt.sign({ _id: id }, process.env.JWT_SECRET as string, {
+    expiresIn: "1d",
+  });
+};
+
 export const login = async (req: Request, res: Response) => {
   const { password } = req.body;
 
   try {
     const admin = await Admin.findOne();
-    if (!admin) {
-      return res.status(404).json({ message: "Admin not found" });
-    }
 
-    const validPassword = await bcrypt.compare(password, admin.password);
-    if (!validPassword) {
-      return res.status(401).json({ message: "Invalid password" });
+    if (admin) {
+      const validPassword = await bcrypt.compare(password, admin.password);
+      if (validPassword) {
+        const token = generateAccessToken(admin._id as string);
+        return res.status(200).json({ token });
+      }
     }
-
-    const token = jwt.sign(
-      { _id: admin._id },
-      process.env.JWT_SECRET as string
-    );
-    res.status(200).json({ token });
   } catch (err) {
     res
       .status(500)
